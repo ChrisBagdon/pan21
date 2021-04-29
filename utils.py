83cd76e4086dba06d42a1d6967eb0b0c921e6b82
ChrisBagdon <chrisbagdon@gmail.com>
2021-04-29 16:42:01 +0200

    Added method for reading in data setes to utils.py

---
+++ utils.py
@@ -0,0 +1,58 @@
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+
+def read_data_into_DFs(folder):
+    """
+    Reads data into a pandas DF for en and es. Features are lang, class, authorID, individual tweets
+    and a fully concatted tweet.
+    :param folder: Path to pan21-author-profiling-training-2021-03-14 dir
+    :return: list of two pandas DF
+    """
+    os.chdir(folder)
+    en_xmls = glob.glob('en/*.xml')
+    es_xmls = glob.glob('es/*.xml')
+
+    authors_DFs = []
+    en_texts = pd.DataFrame
+
+    for xml in en_xmls:
+        xtree = ET.parse(xml)
+        root = xtree.getroot()
+        concat_tweet = ""
+        authorDic = root.attrib
+        authorDic['authorID'] = xml[3:-4]
+
+        for x, doc in enumerate(root.iter('document')):
+            tweet = "tweet" + str(x)
+            authorDic[tweet] = doc.text
+            concat_tweet = concat_tweet + doc.text + " "
+        authorDic['concat'] = concat_tweet
+        authorDF = pd.DataFrame(authorDic, index=[0])
+        authors_DFs.append(authorDF)
+
+    texts = pd.concat(authors_DFs, axis=0)
+
+    authors_DFs = []
+    es_texts = pd.DataFrame
+
+    for xml in es_xmls:
+        xtree = ET.parse(xml)
+        root = xtree.getroot()
+        concat_tweet = ""
+        authorDic = root.attrib
+        authorDic['authorID'] = xml[3:-4]
+
+        for x, doc in enumerate(root.iter('document')):
+            tweet = "tweet" + str(x)
+            authorDic[tweet] = doc.text
+            concat_tweet = concat_tweet + doc.text + " "
+        authorDic['concat'] = concat_tweet
+        authorDF = pd.DataFrame(authorDic, index=[0])
+        authors_DFs.append(authorDF)
+
+    es_texts = pd.concat(authors_DFs, axis=0)
+
+    return [en_texts, es_texts]
